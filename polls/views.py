8c7010d970d0f2e37e3155bff1e6e74566dc230a
joeryvs <joeryvansanten@kpnmail.nl>
2026-06-25 17:09:13 +0200

    Automated testing of simple Logic

--- polls/views.py
+++ polls/views.py
@@ -3,27 +3,29 @@ from django.db.models import F
 from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.views import generic
 from .models import Choice, Question
+from django.utils import timezone
 
 # Create your views here.
-def index(request):
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
     
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    # template = loader.get_template("polls/index.html")
-    context = {"latest_question_list": latest_question_list}
-    # return HttpResponse(template.render(context))
-    return render(request,"polls/index.html",context)
+    def get_queryset(self):
+       return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
-def detail(request, question_id):
-    question = get_object_or_404(Question,pk=question_id)
-    # return HttpResponse("Youre looking for question %s" % question_id)
-    return render(request, "polls/detail.html", {"question":question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
-    # response = "You're looking at the results of question: %s" % question_id
-    # return HttpResponse(response)
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
 
 def vote(request,question_id):
     question = get_object_or_404(Question,pk=question_id)
